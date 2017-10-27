f7cfe2d7d8ca403a2d23913269594f8d72780c58
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-10-27 15:28:04 +0900

    Global event bus access from Store static member actions

--- VueFlux/Sources/VueFlux.swift
+++ VueFlux/Sources/VueFlux.swift
@@ -6,10 +6,6 @@ public struct Dispatcher<State: VueFlux.State> {
         return DispatcherContext.shared.dispatcher(for: State.self)
     }
     
-    public var actions: Actions<State> {
-        return .init(dispatcher: self)
-    }
-    
     private let relay = PublishRelay<State.Action>()
 
     public init() {}
@@ -31,7 +27,11 @@ public class Store<State: VueFlux.State> {
     private let dispatcher = Dispatcher<State>()
     private let disposeBag = DisposeBag()
 
-    public lazy var actions: Actions<State> = dispatcher.actions
+    public static var actions: Actions<State> {
+        return .init(dispatcher: Dispatcher<State>.shared)
+    }
+    
+    public lazy var actions: Actions<State> = .init(dispatcher: dispatcher)
     public lazy var expose: Expose<State> = .init(state: state)
 
     public init(state: State, mutations: State.Mutations, scheduler: ImmediateSchedulerType = SerialDispatchQueueScheduler(qos: .default)) {
@@ -80,28 +80,30 @@ public struct Expose<State: VueFlux.State> {
 
 // MARK: - private
 
-private struct DispatcherContext {
+private final class DispatcherContext {
     static let shared = DispatcherContext()
     
-    private var dispatchers = Atomic<[Identifier: Any]>(value: [:])
+    private var dispatchers = [Identifier: Any]()
+    private let lock = NSLock()
     
     private init() {}
     
     func dispatcher<State: VueFlux.State>(for type: State.Type) -> Dispatcher<State> {
-        return dispatchers.modify { dispatchers in
-            let identifier = Identifier(type: type)
-            if let dispatcher = dispatchers[identifier] as? Dispatcher<State> {
-                return dispatcher
-            }
-            
-            let dispatcher = Dispatcher<State>()
-            dispatchers[identifier] = dispatcher
+        lock.lock()
+        lock.unlock()
+        
+        let identifier = Identifier(type: type)
+        if let dispatcher = dispatchers[identifier] as? Dispatcher<State> {
             return dispatcher
         }
+        
+        let dispatcher = Dispatcher<State>()
+        dispatchers[identifier] = dispatcher
+        return dispatcher
     }
 }
 
-extension DispatcherContext {
+private extension DispatcherContext {
     private struct Identifier: Hashable {
         let hashValue: Int
         
@@ -112,26 +114,5 @@ extension DispatcherContext {
         static func ==(lhs: Identifier, rhs: Identifier) -> Bool {
             return lhs.hashValue == rhs.hashValue
         }
-    }
-}
-
-private final class Atomic<Value> {
-    private let lock = NSLock()
-    private var innerValue: Value
-    
-    var value: Value {
-        get { return modify { $0 } }
-        set { modify { $0 = newValue } }
-    }
-    
-    init(value: Value) {
-        innerValue = value
-    }
-    
-    @discardableResult
-    func modify<Result>(_ action: (inout Value) throws -> Result) rethrows -> Result {
-        lock.lock()
-        defer { lock.unlock() }
-        return try action(&innerValue)
     }
 }
