4e1c4db88d3b1c7cf5989339b809865b13896a31
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-10-30 00:54:58 +0900

    Refactor

--- VueFlux/Sources/VueFlux.swift
+++ VueFlux/Sources/VueFlux.swift
@@ -1,26 +1,6 @@
 import RxSwift
 import RxCocoa
 
-public struct Dispatcher<State: VueFlux.State> {
-    public static var shared: Dispatcher<State> {
-        return DispatcherContext.shared.dispatcher(for: State.self)
-    }
-    
-    private let relay = PublishRelay<State.Action>()
-
-    fileprivate init() {}
-
-    public func dispatch(action: State.Action) {
-        relay.accept(action)
-    }
-
-    public func register(store: Store<State>, on scheduler: ImmediateSchedulerType) -> Disposable {
-        return relay
-            .observeOn(scheduler)
-            .subscribe(onNext: { [weak store] action in store?.dispatch(action: action) })
-    }
-}
-
 public class Store<State: VueFlux.State> {
     private let state: State
     private let mutations: State.Mutations
@@ -60,11 +40,11 @@ public protocol State: class {
 
 public struct Actions<State: VueFlux.State> {
     private let dispatcher: Dispatcher<State>
-
+    
     fileprivate init(dispatcher: Dispatcher<State>) {
         self.dispatcher = dispatcher
     }
-
+    
     public func dispatch(action: State.Action) {
         dispatcher.dispatch(action: action)
     }
@@ -80,6 +60,26 @@ public struct Expose<State: VueFlux.State> {
 
 // MARK: - private
 
+private struct Dispatcher<State: VueFlux.State> {
+    static var shared: Dispatcher<State> {
+        return DispatcherContext.shared.dispatcher(for: State.self)
+    }
+    
+    private let relay = PublishRelay<State.Action>()
+    
+    init() {}
+    
+    func dispatch(action: State.Action) {
+        relay.accept(action)
+    }
+    
+    func register(store: Store<State>, on scheduler: ImmediateSchedulerType) -> Disposable {
+        return relay
+            .observeOn(scheduler)
+            .subscribe(onNext: { [weak store] action in store?.dispatch(action: action) })
+    }
+}
+
 private final class DispatcherContext {
     static let shared = DispatcherContext()
     
@@ -88,11 +88,11 @@ private final class DispatcherContext {
     
     private init() {}
     
-    func dispatcher<State: VueFlux.State>(for type: State.Type) -> Dispatcher<State> {
+    func dispatcher<State: VueFlux.State>(for stateType: State.Type) -> Dispatcher<State> {
         lock.lock()
-        lock.unlock()
+        defer { lock.unlock() }
         
-        let identifier = Identifier(type: type)
+        let identifier = Identifier(for: stateType)
         if let dispatcher = dispatchers[identifier] as? Dispatcher<State> {
             return dispatcher
         }
@@ -107,8 +107,8 @@ private extension DispatcherContext {
     private struct Identifier: Hashable {
         let hashValue: Int
         
-        init<State: VueFlux.State>(type: State.Type) {
-            hashValue = String(reflecting: type).hashValue
+        init<State: VueFlux.State>(for stateType: State.Type) {
+            hashValue = String(reflecting: stateType).hashValue
         }
         
         static func ==(lhs: Identifier, rhs: Identifier) -> Bool {
