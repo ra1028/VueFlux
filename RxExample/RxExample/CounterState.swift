7bfdac1dfe658f74b54bda546199fb978547a51e
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-11-03 07:14:23 +0900

    Update example

--- RxExample/RxExample/CounterState.swift
+++ RxExample/RxExample/CounterState.swift
@@ -3,6 +3,10 @@ import RxSwift
 import RxCocoa
 
 extension Computed where State == CounterState {
+    var excute: Observable<Void> {
+        return state.excute.asObservable()
+    }
+    
     var count: Observable<Int> {
         return state.count.asObservable()
     }
@@ -12,17 +16,29 @@ final class CounterState: State {
     typealias Action = CounterAction
     typealias Mutations = CounterMutations
     
+    fileprivate let max: Int
     fileprivate let count = BehaviorRelay(value: 0)
+    fileprivate let excute = PublishSubject<()>()
+    
+    init(max: Int) {
+        self.max = max
+    }
 }
 
 struct CounterMutations: Mutations {
     func commit(action: CounterAction, state: CounterState) {
         switch action {
+        case .excute:
+            state.excute.onNext(())
+            
         case .increment:
-            state.count.accept(state.count.value + 1)
+            state.count.accept(min(state.max, state.count.value + 1))
 
         case .decrement:
-            state.count.accept(state.count.value - 1)
+            state.count.accept(max(0, state.count.value - 1))
+            
+        case .reset:
+            state.count.accept(0)
         }
     }
 }
--- RxExample/RxExample/CounterViewController.swift
+++ RxExample/RxExample/CounterViewController.swift
@@ -4,26 +4,40 @@ import RxSwift
 import RxCocoa
 
 final class CounterViewController: UIViewController {
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterShadowView: UIView!
+    @IBOutlet private weak var counterContentView: UIView!
+    @IBOutlet private weak var shadowView: UIView!
+    @IBOutlet private weak var excuteView: UIView!
+    
+    @IBOutlet private weak var intervalSlider: UISlider!
+    @IBOutlet private weak var intervalLabel: UILabel!
+    @IBOutlet private weak var intervalContentView: UIView!
+    
     @IBOutlet private weak var incrementButton: UIButton!
     @IBOutlet private weak var decrementButton: UIButton!
-    @IBOutlet private weak var stepper: UIStepper!
-    @IBOutlet private weak var intervalLabel: UILabel!
-    @IBOutlet private weak var countLabel: UILabel!
+    @IBOutlet private weak var resetButton: UIButton!
     
-    private let store = Store<CounterState>(state: .init(), mutations: .init(), executor: .queue(.global()))
+    private let store = Store<CounterState>(state: .init(max: 100), mutations: .init(), executor: .queue(.global()))
     private let disposeBag = DisposeBag()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        bind()
+        configureViews()
     }
 }
 
 private extension CounterViewController {
-    func configure() {
-        let interval = BehaviorRelay<Double>(value: 0)
+    func bind() {
+        let interval = BehaviorRelay<TimeInterval>(value: 0)
         
-        stepper.rx.value
+        intervalSlider.rx.value
+            .map(TimeInterval.init(_:))
             .subscribe(onNext: interval.accept(_:))
             .disposed(by: disposeBag)
         
@@ -34,9 +48,17 @@ private extension CounterViewController {
         
         store.computed.count
             .map(String.init(_:))
-            .bind(to: countLabel.rx.text)
+            .bind(to: counterLabel.rx.text)
             .disposed(by: disposeBag)
-            
+        
+        store.computed.excute
+            .observeOn(MainScheduler.instance)
+            .do(onNext: { [unowned self] in self.excuteView.isHidden = false })
+            .observeOn(ConcurrentDispatchQueueScheduler(queue: .main))
+            .map { true }
+            .bind(to: excuteView.rx.setHiddenAnimated(duration: 0.5))
+            .disposed(by: disposeBag)
+        
         incrementButton.rx.controlEvent(.touchUpInside)
             .subscribe(onNext: { [unowned self] _ in
                 self.store.actions.incrementAcync(after: interval.value)
@@ -48,5 +70,42 @@ private extension CounterViewController {
                 self.store.actions.decrementAcync(after: interval.value)
             })
             .disposed(by: disposeBag)
+        
+        resetButton.rx.controlEvent(.touchUpInside)
+            .subscribe(onNext: { [unowned self] _ in
+                self.store.actions.resetAcync(after: interval.value)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func configureViews() {
+        counterLabel.layer.cornerRadius = 15
+        counterLabel.layer.borderWidth = 7
+        counterLabel.layer.borderColor = view.backgroundColor?.cgColor
+        counterShadowView.layer.shadowOffset = .init(width: 0, height: 2)
+        counterShadowView.layer.shadowOpacity = 0.3
+        counterShadowView.layer.shadowRadius = 2
+        counterContentView.layer.cornerRadius = 15
+        intervalContentView.layer.cornerRadius = 15
+        shadowView.layer.shadowOffset = .init(width: 0, height: 5)
+        shadowView.layer.shadowOpacity = 0.3
+        shadowView.layer.shadowRadius = 5
+        excuteView.layer.cornerRadius = excuteView.bounds.height / 2
+        
+        let buttons: [UIButton] = [incrementButton, decrementButton, resetButton]
+        buttons.forEach { $0.layer.cornerRadius = 10 }
+    }
+}
+
+private extension Reactive where Base: UIView {
+    func setHiddenAnimated(duration: TimeInterval) -> Binder<Bool> {
+        return .init(base) { target, isHidden in
+            UIView.transition(
+                with: target,
+                duration: duration,
+                options: [.transitionCrossDissolve, .overrideInheritedDuration],
+                animations: { target.isHidden = isHidden }
+            )
+        }
     }
 }
