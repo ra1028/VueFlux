016a5173d0cc78b0483c68f494da3c1b9c532a85
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-11-10 02:17:34 +0900

    Tidy

--- Sources/VueFlux.swift
+++ Sources/VueFlux.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 open class Store<State: VueFlux.State> {
     private let state: State
     private let mutations: State.Mutations
@@ -66,5 +68,59 @@ public struct Computed<State: VueFlux.State> {
     
     fileprivate init(state: State) {
         self.state = state
+    }
+}
+
+public struct Executor {
+    public static var immediate: Executor {
+        return .init { execute in execute() }
+    }
+    
+    public static var mainThread: Executor {
+        let innerExecutor = MainThreadInnerExecutor()
+        return .init(innerExecutor.execute(_:))
+    }
+    
+    public static func queue(_ dispatchQueue: DispatchQueue) -> Executor {
+        return .init { execute in dispatchQueue.async(execute: execute) }
+    }
+    
+    private let executor: (@escaping () -> Void) -> Void
+    
+    private init(_ executor: @escaping (@escaping () -> Void) -> Void) {
+        self.executor = executor
+    }
+    
+    public func execute(_ body: @escaping () -> Void) {
+        executor(body)
+    }
+}
+
+private extension Executor {
+    final class MainThreadInnerExecutor {
+        private let executingCount = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        
+        init() {
+            executingCount.initialize(to: 0)
+        }
+        
+        deinit {
+            executingCount.deinitialize()
+            executingCount.deallocate(capacity: 1)
+        }
+        
+        func execute(_ action: @escaping () -> Void) {
+            let count = OSAtomicIncrement32(executingCount)
+            
+            if Thread.isMainThread && count == 1 {
+                action()
+                OSAtomicDecrement32(executingCount)
+            } else {
+                DispatchQueue.main.async {
+                    action()
+                    OSAtomicDecrement32(self.executingCount)
+                }
+            }
+        }
     }
 }
