8260b67bad234a57bb6592510bc87db6fbeefdf2
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-11-14 02:39:12 +0900

    Refactor example

--- RxExample/RxExample/CounterViewController.swift
+++ RxExample/RxExample/CounterViewController.swift
@@ -4,116 +4,52 @@ import RxSwift
 import RxCocoa
 
 final class CounterViewController: UIViewController {
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var intervalLabel: UILabel!
-    @IBOutlet private weak var intervalSlider: UISlider!
-    @IBOutlet private weak var incrementButton: UIButton!
-    @IBOutlet private weak var decrementButton: UIButton!
-    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var counterView: CounterView!
     
     private let store = Store<CounterState>(state: .init(max: 1000), mutations: .init(), executor: .queue(.global()))
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
-        configureViews()
+        configure()
     }
 }
 
 private extension CounterViewController {
-    func bind() {
+    func configure() {
         let interval = BehaviorRelay<TimeInterval>(value: 0)
         
-        intervalSlider.rx.value
+        counterView.intervalSlider.rx.value
             .map(TimeInterval.init(_:))
             .subscribe(onNext: interval.accept(_:))
             .disposed(by: disposeBag)
         
         interval
             .map { "Count after: \(round($0 * 10) / 10)" }
-            .bind(to: intervalLabel.rx.text)
+            .bind(to: counterView.intervalLabel.rx.text)
             .disposed(by: disposeBag)
         
         store.computed.count
             .map(String.init(_:))
-            .bind(to: counterLabel.rx.text)
+            .bind(to: counterView.counterLabel.rx.text)
             .disposed(by: disposeBag)
         
-        incrementButton.rx.controlEvent(.touchUpInside)
+        counterView.incrementButton.rx.controlEvent(.touchUpInside)
             .subscribe(onNext: { [unowned self] _ in
                 self.store.actions.incrementAcync(after: interval.value)
             })
             .disposed(by: disposeBag)
         
-        decrementButton.rx.controlEvent(.touchUpInside)
+        counterView.decrementButton.rx.controlEvent(.touchUpInside)
             .subscribe(onNext: { [unowned self] _ in
                 self.store.actions.decrementAcync(after: interval.value)
             })
             .disposed(by: disposeBag)
         
-        resetButton.rx.controlEvent(.touchUpInside)
+        counterView.resetButton.rx.controlEvent(.touchUpInside)
             .subscribe(onNext: { [unowned self] _ in
                 self.store.actions.resetAcync(after: interval.value)
             })
             .disposed(by: disposeBag)
-    }
-    
-    func configureViews() {
-        func round(view: UIView, corners: UIRectCorner) {
-            let path = UIBezierPath(
-                roundedRect: view.bounds,
-                byRoundingCorners: corners,
-                cornerRadii: .init(width: view.bounds.width, height: view.bounds.height)
-            )
-            let mask = CAShapeLayer()
-            mask.path = path.cgPath
-            view.layer.mask = mask
-        }
-        
-        round(view: incrementButton, corners: [.topRight, .bottomRight])
-        round(view: decrementButton, corners: [.topRight, .bottomRight])
-        round(view: resetButton, corners: [.topLeft, .bottomLeft])
-        
-        counterLabel.font = .monospacedDigitSystemFont(ofSize: 80, weight: .thin)
-        intervalLabel.font = .monospacedDigitSystemFont(ofSize: 20, weight: .light)
-    }
-}
-
-final class GradientView: UIView {
-    @IBInspectable var startColor: UIColor? {
-        didSet { configure() }
-    }
-    
-    @IBInspectable var endColor: UIColor? {
-        didSet { configure() }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        configure()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configure()
-    }
-    
-    override static var layerClass: AnyClass {
-        return CAGradientLayer.self
-    }
-    
-    var gradientLayer: CAGradientLayer? {
-        return layer as? CAGradientLayer
-    }
-}
-
-private extension GradientView {
-    func configure() {
-        guard let gradientLayer = gradientLayer else { return }
-        
-        gradientLayer.colors = [startColor, endColor].flatMap { $0?.cgColor }
-        gradientLayer.startPoint = .init(x: 0.5, y: 0)
-        gradientLayer.endPoint = .init(x: 0.5, y: 1)
     }
 }
