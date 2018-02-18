a239530bdd6681d35a82602ccb94d4e1817e5555
Ryo Aoyama <r.fe51028.r@gmail.com>
2018-02-19 03:38:00 +0900

    Update Storage test

--- Tests/StorageTests.swift
+++ Tests/StorageTests.swift
@@ -3,34 +3,40 @@ import XCTest
 
 final class StorageTests: XCTestCase {
     func testStorage() {
-        var value = 0
-        var storage = Storage<() -> Void>()
+        var storage = Storage<Int>()
         
-        let f1 = {
-            value += 1
+        let additionValue1 = 1
+        let additionValue2 = 2
+        
+        let key1 = storage.add(additionValue1)
+        let key2 = storage.add(additionValue2)
+        
+        XCTAssertEqual(storage[0], additionValue1)
+        XCTAssertEqual(storage[1], additionValue2)
+        
+        var targetValue1 = 0
+        for additionValue in storage {
+            targetValue1 += additionValue
         }
         
-        let f2 = {
-            value += 10
-        }
-        
-        let key1 = storage.add(f1)
-        let key2 = storage.add(f2)
-        
-        for f in storage { f() }
-        
-        XCTAssertEqual(value, 11)
+        XCTAssertEqual(targetValue1, additionValue1 + additionValue2)
         
         storage.remove(for: key1)
         
-        for f in storage { f() }
+        var targetValue2 = 0
+        for additionValue in storage {
+            targetValue2 += additionValue
+        }
         
-        XCTAssertEqual(value, 21)
+        XCTAssertEqual(targetValue2, additionValue2)
         
         storage.remove(for: key2)
         
-        for f in storage { f() }
+        var targetValue3 = 0
+        for additionValue in storage {
+            targetValue3 += additionValue
+        }
         
-        XCTAssertEqual(value, 21)
+        XCTAssertEqual(targetValue3, 0)
     }
 }
