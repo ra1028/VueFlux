4d795f49e11380eac9b6916841bc28eb210f482e
Ryo Aoyama <r.fe51028.r@gmail.com>
2018-03-08 20:11:15 +0900

    Move CancelableProcedure to CommonInternal

--- CommonInternal/CancelableProcedure.swift
+++ CommonInternal/CancelableProcedure.swift
@@ -1,8 +1,8 @@
 /// Encapsulate the function and make it cancelable.
 /// A function will not be executed after canceling, except already in progress execution.
-public final class CancelableProcedure<Value> {
+final class CancelableProcedure<Value> {
     /// A Bool value indicating whether canceled.
-    public var isCanceled: Bool {
+    var isCanceled: Bool {
         return _isCanceled.value
     }
     
@@ -13,7 +13,7 @@ public final class CancelableProcedure<Value> {
     ///
     /// - Parameters:
     ///   - execute: A function to be executed by calling `execute(with:)` until canceled.
-    public init(_ execute: @escaping (Value) -> Void) {
+    init(_ execute: @escaping (Value) -> Void) {
         _execute = execute
     }
     
@@ -21,23 +21,23 @@ public final class CancelableProcedure<Value> {
     ///
     /// - Parameters:
     ///   - value: A value to be pass to specified function.
-    public func execute(with value: @autoclosure () -> Value) {
+    func execute(with value: @autoclosure () -> Value) {
         guard !isCanceled, let execute = _execute else { return }
         execute(value())
     }
     
     /// Cancel the specified function.
     /// Cancellation does not affect already in progress execution.
-    public func cancel() {
+    func cancel() {
         guard _isCanceled.compareAndSwapBarrier(old: false, new: true) else { return }
         _execute = nil
     }
 }
 
-public extension CancelableProcedure where Value == Void {
+extension CancelableProcedure where Value == Void {
     /// Synchronously execute the specified function.
     @inline(__always)
-    public func execute() {
+    func execute() {
         self.execute(with: ())
     }
 }
